ef7543bccf0fc68904299c6d37582704b6399b84
executivereader <executivereader@users.noreply.github.com>
2015-07-29 17:54:10 -0400

    Update article-mapper.py

--- article-mapper.py
+++ article-mapper.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
     while 1:
         raw_events = client.dataminr.articles.find().sort("eventTime", -1).limit(10)
         news_events = client.raw_articles.news.find({"pubDate": {"$ne": "None"}}).sort("pubDate", -1).limit(10)
-        reuters_events = client.tr.articles.find({"newsMessage.itemSet.newsItem.itemMeta.versionCreated": {"$exists": True}}).sort("newsMessage.itemSet.newsItem.itemMeta.versionCreated", -1).limit(10000)
+        reuters_events = client.tr.articles.find({"newsMessage.itemSet.newsItem.contentSet.inlineXML.html.body.p": {"$exists": True}}).sort("newsMessage.itemSet.newsItem.itemMeta.versionCreated", -1).limit(10000)
         print "Processing Dataminr articles"
         process_dataminr_events(raw_events, client)
         print "Processing news articles"
