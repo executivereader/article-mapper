a7a5ac581abde3e565ae02e7b7d63858793b3669
executivereader <executivereader@users.noreply.github.com>
2015-07-29 15:45:45 -0400

    update

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # the contents of this file should be put in the user data field
+sudo apt-get update
 sudo apt-get install -y git
 cd /home/ubuntu/
 sudo git clone https://github.com/executivereader/mongo-startup.git
